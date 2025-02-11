01540fdb62ea8566e3af8e6336c58bb32e9d7d05
9324868108 <166347451+9324868108@users.noreply.github.com>
2025-02-11 21:29:01 +0530

    Create config.js

---
+++ services/config.js
@@ -0,0 +1,7 @@
+const config = {
+    API_KEY: '1604223ff3msh6caecf21f6f105dp1ff9b6jsn4d2ce1968ac0',
+    API_BASE_URL: 'https://indian-railway-irctc.p.rapidapi.com',
+    API_TIMEOUT: 10000 // 10 seconds
+};
+
+export default config;
